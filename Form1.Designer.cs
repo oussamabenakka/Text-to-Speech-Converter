eee1502838f7abe1f47eea048295e8873151b049
OUSSMA BENAKKA <BENAKKA.OUSSAMA@ofppt-edu.ma>
2021-01-05 23:30:57 +0100

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace Text_to_Speech_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.trackBarvalum = new System.Windows.Forms.TrackBar();
+            this.trackBarSpeed = new System.Windows.Forms.TrackBar();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarvalum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSpeed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(14, 86);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(889, 402);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 51);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Speak";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(152, 6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 51);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Pause";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(292, 6);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(134, 51);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Resume";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(712, 28);
+            this.comboBox1.MaxDropDownItems = 20;
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(355, 29);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(920, 467);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 21);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Volum";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1006, 467);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 21);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Speed";
+            // 
+            // trackBarvalum
+            // 
+            this.trackBarvalum.Location = new System.Drawing.Point(924, 87);
+            this.trackBarvalum.Maximum = 100;
+            this.trackBarvalum.Minimum = 1;
+            this.trackBarvalum.Name = "trackBarvalum";
+            this.trackBarvalum.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBarvalum.Size = new System.Drawing.Size(45, 377);
+            this.trackBarvalum.TabIndex = 7;
+            this.trackBarvalum.Value = 1;
+            // 
+            // trackBarSpeed
+            // 
+            this.trackBarSpeed.LargeChange = 2;
+            this.trackBarSpeed.Location = new System.Drawing.Point(1009, 87);
+            this.trackBarSpeed.Name = "trackBarSpeed";
+            this.trackBarSpeed.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBarSpeed.Size = new System.Drawing.Size(45, 377);
+            this.trackBarSpeed.TabIndex = 8;
+            this.trackBarSpeed.Value = 1;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(432, 6);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(134, 51);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Record";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(124, 21);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Enter your text";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(572, 6);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(134, 51);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Exit";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(850, 4);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 21);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "language";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1072, 502);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.trackBarSpeed);
+            this.Controls.Add(this.trackBarvalum);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarvalum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSpeed)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar trackBarvalum;
+        private System.Windows.Forms.TrackBar trackBarSpeed;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Speech;
+
+namespace Text_to_Speech_Converter
+{
+    public partial class Form1 : Form
+    {
+        SpeechSynthesizer speech;
+        public Form1()
+        {
+            InitializeComponent();
+            speech = new SpeechSynthesizer();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            button3.Enabled = false;
+            trackBarvalum.Value = 100;
+            foreach (var voice in speech.GetInstalledVoices())
+            {
+                comboBox1.Items.Add(voice.VoiceInfo.Name);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(richTextBox1.Text!="")
+            {
+                if(comboBox1.Text=="")
+                {
+                    MessageBox.Show("sélection un langue", "", MessageBoxButtons.OK);
+                }
+                else
+                {
+                    button2.Enabled = true;
+                    button3.Enabled = true;
+                    speech.Rate = trackBarSpeed.Value;//set speed
+                    speech.Volume = trackBarvalum.Value;//set valume
+                    speech.SelectVoice(comboBox1.Text);
+                    speech.SpeakAsync(richTextBox1.Text);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (speech.State == SynthesizerState.Speaking)
+                speech.Pause();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (speech.State == SynthesizerState.Paused)
+                speech.Resume();
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text == "")
+            {
+                MessageBox.Show("Enter your text first", "", MessageBoxButtons.OK);
+            }
+            else
+            {
+                SpeechSynthesizer ss = new SpeechSynthesizer();
+                SaveFileDialog savefile = new SaveFileDialog();
+                savefile.Filter = "Wave Files | *.wav";
+                savefile.ShowDialog();
+                ss.SetOutputToWaveFile(savefile.FileName);
+                ss.Rate = trackBarSpeed.Value;
+                ss.Volume = trackBarvalum.Value;
+                ss.SelectVoice(comboBox1.Text);
+                ss.SpeakAsync(richTextBox1.Text);
+                MessageBox.Show("Recording Completed...", "");
+            }
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+          DialogResult rep=MessageBox.Show("Are you sure", "verification", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if(rep==DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
